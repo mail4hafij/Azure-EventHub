e5840e6f0bf7e62e24143273727d2b03aca85310
Rahman Mohammad Hafijur <mohammad-hafijur.rahman@bostad.uppsala.se>
2020-11-28 18:51:54 +0100

    adding storage account connection string

--- ReceiveEvents/Program.cs
+++ ReceiveEvents/Program.cs
@@ -12,15 +12,15 @@ namespace ReceiveEvents
     {
         private const string ConnectionString = "your_event_hub_connection_string";
         private const string EventHubName = "your_event_hub_name";
-        private const string BlobStorageConnectionString = "your_blob_connection_string";
-        private const string BlobContainerName = "your_container_name";
+        private const string StorageAccountConnectionString = "your_storage_account_connection_string";
+        private const string BlobContainerName = "your_blob_container_name";
         
         static async Task Main()
         {
             string consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
 
             // Event client uses a blob storage to save the checkpoint. 
-            BlobContainerClient storageClient = new BlobContainerClient(BlobStorageConnectionString, BlobContainerName);
+            BlobContainerClient storageClient = new BlobContainerClient(StorageAccountConnectionString, BlobContainerName);
             EventProcessorClient processorClient = new EventProcessorClient(storageClient, consumerGroup, ConnectionString, EventHubName);
 
             // Registering the handlers
@@ -28,21 +28,19 @@ namespace ReceiveEvents
             processorClient.ProcessErrorAsync += ProcessErrorHandler;
 
             await processorClient.StartProcessingAsync();
-            // wait for 10 sec
-            await Task.Delay(TimeSpan.FromSeconds(10));
             await processorClient.StopProcessingAsync();
         }
 
         static async Task ProcessEventHandler(ProcessEventArgs eventArgs)
         {
-            Console.WriteLine("Event received: {0}", Encoding.UTF8.GetString(eventArgs.Data.Body.ToArray()));
+            Console.WriteLine("\tEvent received: {0}", Encoding.UTF8.GetString(eventArgs.Data.Body.ToArray()));
             // Important: make sure the save the checkpoint.
             await eventArgs.UpdateCheckpointAsync(eventArgs.CancellationToken);
         }
 
         static Task ProcessErrorHandler(ProcessErrorEventArgs eventArgs)
         {
-            Console.WriteLine($"Exception in partition '{ eventArgs.PartitionId}': an unhandled exception has occurred.");
+            Console.WriteLine($"\tException in partition '{ eventArgs.PartitionId}': an unhandled exception has occurred.");
             Console.WriteLine(eventArgs.Exception.Message);
             return Task.CompletedTask;
         } 
